2c237e89b073fdb467c61999a64e7d3aacb82219
Dom-Napoli <Dom-Napoli@users.noreply.github.com>
2023-12-12 11:14:49 -0800

    Loops completed. All objects are assigned accordingly in card.js.

--- assets/js/card.js
+++ assets/js/card.js
@@ -6,25 +6,54 @@ const urlParams = new URLSearchParams(queryString);
 console.log(urlParams);
 //code "where to search"
 
-const cardId = parseInt(urlParams.get("cards"));
+const cardId = parseInt(urlParams.get("card"));
 console.log(typeof cardId);
-//code "provide/query all card data"
+//code "provide/query all cardIds"
 
-console.log(cards);
-let selectedCardsObjects
-//VAR | assigning name to card-objects.js/file-array to cardObjects.
-// required in order for js code to "pass" all of the file-array's parameters to each individual card's html page. 
-//The id numbers assigned in card-objects.js file/array ensure that each individual card's html page is populated with the correct parameters (corresponding elements/data).
+//console.log to confirm card-objects.js location - "all-123 objects accessed"
+console.log(cardObjects);
+let selectedCardsObjects;
+//VAR | assigned name "selectCardsObjects" 
+//VAR | loop used to access objects and assign to individual card
+//VAR | using card_id located in selectedCardObjects = (card-objects.js array) 
+//VAR | will then gather and assign all objects in card-id and assign to individual cards
 
-//loop created for JS to access & pull parameters from the card-objects.js file/array for each card and pass to each card's html page.
-for (let i = 0; i < cardObjects.length; i++){
+for (let i = 0; i < cardObjects.length; i++) {
+    //console.log of cardObjects[i] confirms array accessed and assigned
     console.log(cardObjects[i]);
-    //console.log(cardObjects[i]); - confirms entire array indexed.
     if (cardObjects[i].id === cardId) {
         selectedCardsObjects = cardObjects[i];
-
     }
-}
-//console.log is returning as undefined?
+};
+//console.log confirmed when click from main to individual card page
 console.log(selectedCardsObjects);
 
+let selectedDeck;
+//VAR | assigned name "selectedDeck"
+//VAR | loop used to access all three decks by name
+//VAR | using deck id located in selectedCardObjects = (decks-cards.js array)
+//VAR | will then gather and assign deck_id to individual cards
+for (let i = 0; i < decks.length; i++) {
+    //console.log of decks[i] confirms all three decks accessed in array
+    console.log(decks[i]);
+    if (decks[i].id  === selectedCardsObjects.deck_id) {
+        selectedDeck = decks[i];
+    }
+}
+// console.log confirms deck matches with card number
+console.log(selectedDeck);
+
+let selectedCard;
+//VAR | assigned name "selectedCard"
+//VAR | loop used to access all three decks by name
+//VAR | using deck id located in selectedCardObjects = (decks-cards.js array)
+//VAR | will then gather and assign card_id to individual cards
+for (let i = 0; i < cards.length; i++){
+    //console.log of decks[i] confirms all three decks accessed in array
+    console.log(cards[i]);
+    if(cards[i].id === selectedCardsObjects.card_id){
+        selectedCard = cards[i];
+    }
+}
+// console.log confirms card matches with card objects
+console.log(selectedCard);
